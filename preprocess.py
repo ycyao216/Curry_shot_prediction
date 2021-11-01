db036ae174ffc8972a66fe9a3c40afbec3f5342f
Lukas Williams <warhawk927@live.com>
2021-11-01 16:00:23 -0700

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -20,15 +20,18 @@ def process_data(data_path, player, scaling_features, train_path, valid_path, te
     for col in scaling_features:
         df[col] = (df[col] - df[col].mean()) / df[col].std()
 
+    # Encode labels to Missed = 0 and Made = 1
     df['SHOT_RESULT'] = df['SHOT_RESULT'].replace({'missed': 0, 'made': 1})
 
+    # Split to train, validation, and test data with a 80/10/10 split
     train, test = train_test_split(df, test_size=0.1, random_state=42)
     train, valid = train_test_split(train, test_size=1/9, random_state=42)
-    
+
+    # Select columns for output
     cols = scaling_features
     cols.append('SHOT_RESULT')
 
-    # Export features and labels to csv
+    # Export to csv
     train[cols].to_csv(train_path)
     valid[cols].to_csv(valid_path)
     test[cols].to_csv(test_path)
